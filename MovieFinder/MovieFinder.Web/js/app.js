e28469cf1c781672050fbf4f4302b0e9d3eae0b4
Elizabeth Maraan <emaraan@gmail.com>
2016-02-03 14:00:53 -0800

    finished basic project and added results filter

---
+++ MovieFinder/MovieFinder.Web/js/app.js
@@ -0,0 +1,29 @@
+﻿angular.module('app', []);
+
+angular.module('app').controller('indexController', function ($scope, $http) {
+
+    $scope.searchTerm = '';
+
+    console.log('issuing http get');
+    $scope.searchForMovie = function () {
+        $http.get('http://www.omdbapi.com/?type=movie&s=' + $scope.searchTerm)
+                .success(function (data) {
+                    $scope.movies = data.Search;
+                    console.log(data)
+                })
+            .error(function (error) {
+                alert(error);
+            });
+    };
+
+    $scope.searchForTvShow = function () {
+        $http.get('http://www.omdbapi.com/?type=series&s=' + $scope.searchTerm)
+                .success(function (data) {
+                    $scope.movies = data.Search;
+                    console.log(data)
+                })
+            .error(function (error) {
+                alert(error);
+            });
+    };
+});
